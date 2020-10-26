5998cee66343917687eb779b3f362c5803bf9647
Nerovnyy Yan <yan.krivoy@gmail.com>
2020-10-26 05:01:43 +0300

    first commit

---
+++ src/components/app-header/index.js
@@ -0,0 +1,4 @@
+
+import AppHeader from "./app-header";
+
+export default AppHeader;
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './post-status-filter.css';
+
+const PostStatusFilter = ()=>{
+    return(
+        <div className="btn-group">
+            <button>
+
+            </button>
+            <button>
+
+            </button>
+
+        </div>
+    )
+}
+export default PostStatusFilter;
